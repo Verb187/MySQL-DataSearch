87ac8fcaa201b8cefe9a78d9957e92adab24a3aa
Verb <58372226+Verb178@users.noreply.github.com>
2022-07-01 09:18:30 +0200

    0.0.2 Branch MySQL-DataSearch Tickets Gestion
    - Ajout du Login with MySQL 100%

--- MySQL DataSearch/Forms/frmLogin.cs
+++ MySQL DataSearch/Forms/frmLogin.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MySQL_DataSearch.DataAccess;
+using SqlKata;
+using SqlKata.Execution;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,14 +28,34 @@ namespace MySQL_DataSearch.Forms
 
         private void bunifuButton1_Click(object sender, EventArgs e)
         {
-            var form = Application.OpenForms.OfType<frmMain>().FirstOrDefault();
-            if (form == null)
+
+            var user = DataAccess.DBcontext.Db()
+            .Query("users")
+            .Where("email", txtEmail.Text)
+            .Where("password", txtpass1.Text)
+            .FirstOrDefault<Event.User>();
+                
+            if (user == null)
             {
-                form = new frmMain();
+                MessageBox.Show("Invalid login details");
             }
-            form.Show();
-            frmLogin fLogin = new frmLogin();
-            fLogin.Hide();
+            else
+            {
+                var form = Application.OpenForms.OfType<frmMain>().FirstOrDefault();
+                if (form == null)
+                {
+                    form = new frmMain();
+                }
+                var Form2 = new frmMain();
+                this.Visible = false;
+                Form2.Show();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+                this.Hide();
         }
     }
 }
